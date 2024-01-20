cbc4990aa7c18e73ce747dd73195507a5a1b4658
Creator-CB <117445826+Creator-CB@users.noreply.github.com>
2024-01-20 19:23:19 +0200

    Update run_babylond.sh

--- run_babylond.sh
+++ run_babylond.sh
@@ -1,7 +1,17 @@
-.#!/bin/bash
+#!/bin/bash
 
 # Set the full path to the babylond executable
 BABYLOND="/usr/local/bin/babylond"
 
-# Run the babylond command and capture both stdout and stderr
-$BABYLOND tx epoching delegate bbnvaloper1ak6xmt8dvwxhsqwvz3mduwnplrl75j7dnlr0r5 1000000ubbn --from wallet --chain-id bbn-test-2 --gas-adjustment 1.4 --gas auto --fees 10ubbn -y
+# Set the log file path
+LOG_FILE="/root/babylond_log.log"
+
+# Add timestamp to the log entries
+echo "-----------------------------------" >> "$LOG_FILE"
+date >> "$LOG_FILE"
+
+# Run the babylond command and capture both stdout and stderr, and append to the log file
+$BABYLOND tx epoching delegate bbnvaloper1ak6xmt8dvwxhsqwvz3mduwnplrl75j7dnlr0r5 1000000ubbn --from wallet --chain-id bbn-test-2 --gas-adjustment 1.4 --gas auto --fees 10ubbn -y >> "$LOG_FILE" 2>&1
+
+# Display a message in the log file indicating the end of the script
+echo "Script execution completed." >> "$LOG_FILE"
